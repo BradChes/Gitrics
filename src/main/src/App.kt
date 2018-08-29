27a5ac00cefeb181c947dc6fb7eeea0e51803447
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2018-08-29 09:05:58 +0100

    Initial commit

---
+++ src/main/src/App.kt
@@ -0,0 +1,14 @@
+fun main(args : Array<String>) {
+    App()
+}
+
+class App {
+
+    init {
+        printStatement()
+    }
+
+    fun printStatement() {
+        println("Hello gitrics")
+    }
+}
---
+++ src/test/src/AppTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+
+class AppTest {
+
+    @Test
+    fun testSomething() {
+
+    }
+}
