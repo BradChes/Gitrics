30aa7dddb587116c80dbfba9a705d12348e53caa
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2018-10-16 09:57:09 +0100

    Update src/main/java/jGit/JGitService.kt
    Minor refactor of other methods as well as a base method on for getting when branches were first made.

--- src/main/java/jGit/JGitService.kt
+++ src/main/java/jGit/JGitService.kt
@@ -3,12 +3,17 @@ package jGit
 import java.io.File.*
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.api.ListBranchCommand.*
+import org.eclipse.jgit.lib.Ref
+import org.eclipse.jgit.revwalk.RevWalk
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
+import org.eclipse.jgit.treewalk.TreeWalk
+
 
 
 class JGitService(remoteRepositoryUri: String) {
 
     private var git: Git
+    private var branchCall: List<Ref>
 
     init {
         System.out.println("launching service, please stand by...")
@@ -23,24 +28,29 @@ class JGitService(remoteRepositoryUri: String) {
                 .call()
 
         System.out.println("Having repository: " + git.repository.directory)
+        System.out.println("Using repository: " + git.repository.directory)
 
-        getListOfRemoteBranches()
+        branchCall = git.branchList().setListMode(ListMode.REMOTE).call()
+
         getNumberOfRemoteBranches()
+        getListOfRemoteBranches()
+        getWhenBranchesWereFirstMade()
 
         System.out.println("Stopping service...")
     }
 
-    private fun getListOfRemoteBranches() {
-        val branchCall = git.branchList().setListMode(ListMode.REMOTE).call()
+    private fun getNumberOfRemoteBranches() = System.out.println("Size of remote branches: ${branchCall.size}")
 
+    private fun getListOfRemoteBranches() {
+        System.out.println("Full list of remote branches:")
         for (branchRef in branchCall) {
-            System.out.println(branchRef.name)
+            System.out.println("- ${branchRef.name}")
         }
     }
 
-    private fun getNumberOfRemoteBranches() {
-        val branchCall = git.branchList().setListMode(ListMode.REMOTE).call()
+    private fun getWhenBranchesWereFirstMade() {
+        for (branchRef in branchCall) {
 
-        System.out.println(branchCall.size)
+        }
     }
 }
