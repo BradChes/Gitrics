5e94363eda6afea92f2a8c7bd2e433d9eb245ae0
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2019-01-15 12:35:38 +0000

    removed imports
    that are not used.

--- src/main/java/application/App.kt
+++ src/main/java/application/App.kt
@@ -1,11 +1,9 @@
 package application
 
 import controllers.BranchesController
-import models.Account
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import services.JGitService
 
 
 @EnableAutoConfiguration
