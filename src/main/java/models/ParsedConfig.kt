2611ff0b0de513f46099e18fdd583532e7f401a7
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2019-04-09 14:33:18 +0100

    Added stale definition to parsedConfig.

--- src/main/java/models/ParsedConfig.kt
+++ src/main/java/models/ParsedConfig.kt
@@ -4,4 +4,5 @@ data class ParsedConfig(val username: String,
                         val accessToken: String,
                         val repoUrl: String,
                         val branchMinimum: Int,
-                        val branchMaximum: Int)
+                        val branchMaximum: Int,
+                        val staleDefinition: Int)
