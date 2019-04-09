ce71dd0df102c08b73e0eee31f596071ca6af3bf
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2019-04-09 15:32:32 +0100

    ParsedConfig now takes a list instead of a string.
    Config Reader only takes the first value and then applys it to the account object.

--- src/main/java/models/ParsedConfig.kt
+++ src/main/java/models/ParsedConfig.kt
@@ -2,7 +2,7 @@ package models
 
 data class ParsedConfig(val username: String,
                         val accessToken: String,
-                        val repoUrl: String,
+                        val repoUrls: List<String>,
                         val branchMinimum: Int,
                         val branchMaximum: Int,
                         val staleDefinition: Int)
