e3cd5d4af91361c52e46742ef70462ab708a64e3
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2019-04-10 15:53:04 +0100

    Added branchesAverageLifetimeMinimumThreshold to options

--- src/main/java/models/configs/Options.kt
+++ src/main/java/models/configs/Options.kt
@@ -3,4 +3,6 @@ package models.configs
 data class Options(val repoPath: String,
                    val branchMinimum: Int,
                    val branchMaximum: Int,
+                   val branchesAverageLifetimeMinimumThreshold: Int,
+                   val branchesAverageLifetimeMaximumThreshold: Int,
                    val staleDefinition: Int)
